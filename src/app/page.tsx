c79cc0f6bc39a6e372fb9644272b705788cebd3f
Danilo Zabala II <dczii@live.com>
2025-06-18 11:20:08 +0800

    Add style and inputs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,22 +8,44 @@ import remarkGfm from "remark-gfm";
 
 export default function Home() {
   const [result, setResult] = useState("");
+  const [numberOfLevels, setNumberOfLevels] = useState(0);
+  const [numberOfSections, setNumberOfSections] = useState(0);
   const [loading, setLoading] = useState(false);
 
   const generateClass = async () => {
     setLoading(true);
-    const res = await axios.get("/api/generate");
+    const res = await axios.post("/api/generate", {
+      levels: numberOfLevels,
+      sections: numberOfSections,
+    });
     setResult(res.data.response);
     setLoading(false);
   };
 
-  useEffect(() => {
-    generateClass();
-  }, []);
-
   return (
-    <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
-      Class AI
+    <div className='flex flex-col gap-4 justify-center items-center'>
+      <div>Class AI </div>
+      Number of Levels:
+      <input
+        type='number'
+        className='border-2 border-gray-300 rounded-md p-2 w-20'
+        value={numberOfLevels}
+        onChange={(e) => setNumberOfLevels(Number(e.target.value))}
+      />
+      Number of Sections per Level:{" "}
+      <input
+        type='number'
+        className='border-2 border-gray-300 rounded-md p-2 w-20'
+        value={numberOfSections}
+        onChange={(e) => setNumberOfSections(Number(e.target.value))}
+      />
+      <button
+        className='bg-blue-500 text-white p-2 rounded-md cursor-pointer disabled:bg-gray-200 disabled:text-black'
+        onClick={generateClass}
+        disabled={numberOfLevels === 0 && numberOfSections === 0}
+      >
+        Generate Class Schedule
+      </button>
       {loading && (
         <motion.div
           animate={{ rotate: 360 }}
